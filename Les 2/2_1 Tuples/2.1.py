2ade2cf7354d3c9911a74cafa0df07035b524189
Tomersardjoe <tomersardjoe@hotmail.com>
2017-09-07 10:14:24 +0200

    2.1 finished

--- Les 2/2_1 Tuples/2.1.py
+++ Les 2/2_1 Tuples/2.1.py
@@ -1,1 +1,6 @@
-#Vragen in de les
+letters = ('A','A','A','A','B','B','B','C','C','C','C','C')
+a = letters.count('A')
+b = letters.count('B')
+c = letters.count('C')
+letter = [a,b,c]
+print(letter)
