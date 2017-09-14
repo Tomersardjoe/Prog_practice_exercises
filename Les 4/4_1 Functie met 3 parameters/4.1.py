606c0e3b42343e1144ebdbe1d3f71f82daf763c0
Tomersardjoe <tomersardjoe@hotmail.com>
2017-09-14 12:58:38 +0200

    Les 4 tot 4_3

---
+++ Les 4/4_1 Functie met 3 parameters/4.1.py
@@ -0,0 +1,5 @@
+def som(getal1, getal2, getal3):
+    answer = getal1 + getal2 + getal3
+    return(answer)
+
+print(som(4,2,6))
---
+++ Les 4/4_2 Functie met list parameter/4.2.py
@@ -0,0 +1,5 @@
+def som(getallenLijst):
+    answer = sum(getallenLijst)
+    return(answer)
+
+print(som([1,2,6,9]))
