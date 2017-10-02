01025fd5443066665698ac269d87614479e47f2f
Tomersardjoe <tomersardjoe@hotmail.com>
2017-10-02 16:59:47 +0200

    Bijgewerkt tot 6.3

--- Les 5/5_2 Files lezen/5.2.py
+++ Les 5/5_2 Files lezen/5.2.py
@@ -1,7 +1,12 @@
 fileWords = open('Kaartnummers.txt','r')
-print(fileWords.read().split(','))
-#print(fileWords.read())
 
+for line in fileWords:
+    nummer = line.split(', ')
+    if '\n' in nummer[1]:
+        naampersoon = nummer[1].replace('\n','')
+    else: continue
+    regel = naampersoon + ' heeft kaartnummer: ' + nummer[0]
+    print(regel)
 
 
 fileWords.close()
--- Les 6/6_2 Lists/6.2.py
+++ Les 6/6_2 Lists/6.2.py
@@ -1,2 +1,10 @@
 list = eval(input('Geef een lijst met minimaal 10 strings: '))
-print(list)
+
+newlist = []
+
+for word in list:
+    if len(word) == 4:
+        newlist.append(word)
+    else:
+        continue
+print('De nieuw-gemaakte lijst met alle vier-letter strings is: ' + str(newlist))
