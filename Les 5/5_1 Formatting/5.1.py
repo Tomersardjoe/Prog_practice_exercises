aca607d41dc47e8cb90936960aeec2e583e00076
Tomersardjoe <tomersardjoe@hotmail.com>
2017-10-08 16:45:55 +0200

    Touchup on 5.1

--- Les 5/5_1 Formatting/5.1.py
+++ Les 5/5_1 Formatting/5.1.py
@@ -1,9 +1,13 @@
 def convert(gradenCelcius):
-    result = int(gradenCelcius*1.8+32)
-    print(result)
+    result = float(gradenCelcius*1.8+32)
+    return result
 
 def table():
     for temp in range(-30,50,10):
-        convert(temp)
+        celsius = float(temp)
+        fahrenheit = convert(temp)
+        print('{:>5}   {:>5}'.format(fahrenheit, celsius))
 
+
+print('{:>4}   {:>4}'.format('F', 'C'))
 table()
